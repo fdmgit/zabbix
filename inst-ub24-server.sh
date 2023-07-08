7a4d49a1104b981a43448dee5d316fe3aeb00043
Flavio Del Mestre <flavio@perunis.ch>
2023-07-08 18:04:32 +0200

    Create inst-ub24-server.sh

---
+++ inst-ub24-server.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+############################
+# Install Zabbix repository 
+############################
+
+wget https://repo.zabbix.com/zabbix/6.4/debian/pool/main/z/zabbix-release/zabbix-release_6.4-1+debian12_all.deb
+dpkg -i zabbix-release_6.4-1+debian12_all.deb
+apt update 
+
+#########################################
+# Install Zabbix server, frontend, agent 
+#########################################
+
+apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent
+
+
+##########################
+# Create initial database  
+##########################
+
+rootpw=$1
+userpw=$1
+
+mysql -uroot -p"$rootpw" <<INITDB
+create database zabbix character set utf8mb4 collate utf8mb4_bin;
+create user zabbix@localhost identified by "$userpw";
+grant all privileges on zabbix.* to zabbix@localhost;
+set global log_bin_trust_function_creators = 1;
+INITDB
+
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p"$userpw" zabbix 
+
+mysql -uroot -p"$rootpw" -e "set global log_bin_trust_function_creators = 0;"
+
+cd /etc/zabbix
+sed -i "s/# DBPassword=/DBPassword=$rootpw/" zabbix_server.conf
+
+systemctl restart zabbix-server zabbix-agent apache2
+systemctl enable zabbix-server zabbix-agent 
+
+echo DONE........
