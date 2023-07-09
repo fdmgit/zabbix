dec04792ac49d3480879e83656f150aeb16e0208
Flavio Del Mestre <flavio@perunis.ch>
2023-07-09 08:55:52 +0200

    Update inst-ub24-server-arm.sh

--- inst-ub24-server-arm.sh
+++ inst-ub24-server-arm.sh
@@ -4,6 +4,7 @@
 # Install Zabbix repository 
 ############################
 
+cd /root
 wget https://repo.zabbix.com/zabbix/6.4/ubuntu-arm64/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
 dpkg -i zabbix-release_6.4-1+ubuntu22.04_all.deb
 apt update 
@@ -29,6 +30,8 @@ grant all privileges on zabbix.* to zabbix@localhost;
 set global log_bin_trust_function_creators = 1;
 INITDB
 
+echo -e 'populated DB. Wait.....\n'
+
 zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql --default-character-set=utf8mb4 -uzabbix -p"$userpw" zabbix 
 
 mysql -uroot -p"$rootpw" -e "set global log_bin_trust_function_creators = 0;"
@@ -39,4 +42,11 @@ sed -i "s/# DBPassword=/DBPassword=$rootpw/" zabbix_server.conf
 systemctl restart zabbix-server zabbix-agent apache2
 systemctl enable zabbix-server zabbix-agent 
 
-echo DONE........
+cd /root
+rm zabbix-release_6.4-1+ubuntu22.04_all.deb
+
+echo -e 'DONE........\n'
+sleep 2
+echo -e 'Rebooting\n'
+sleep 5
+reboot
